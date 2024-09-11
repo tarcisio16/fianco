57fad9db9a3eb181d77a27a8d4b8b332bf821eab
tarcisio16 <160622023+tarcisio16@users.noreply.github.com>
2024-09-11 22:17:03 +0200

    parameters

---
+++ parameters.py
@@ -0,0 +1,20 @@
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (200, 200, 200)
+
+TYPE_OF_GAME =  0 # 0: "human VS human" 
+                   # 1: "ai VS ai" 
+                   # 2: "ai vs human"
+DELAY_AI = 0.2
+
+WIDTH, HEIGHT = 700, 700
+MARGIN = 70
+GRID_SIZE = 500
+CELL_SIZE = GRID_SIZE // 8
+FONT_SIZE = 30
+BOARD_SIZE = 9
+LETTERS = "ABCDEFGHI"
+
+WHITE_PIECE = 1
+BLACK_PIECE = 2
